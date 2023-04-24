fb44c9bd274dbea549d58f722b0ef3a2f595624c
Devilpacks <mangyr97@mail.ru>
2023-04-24 13:20:04 +0300

    add prototype

---
+++ Creational/Prototype/index.ts
@@ -0,0 +1,13 @@
+const zombie = {
+    eatBrains() {
+      return 'yum 🧠';
+    }
+  }
+  
+  const chad = Object.create(zombie, { name: { value: 'chad'} });
+  
+  chad.__proto__;
+  Object.getPrototypeOf(chad);
+  
+  const babyChad = Object.create(chad, { baby: { value: true } });
+  
