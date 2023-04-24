f6850734db40828986ca0715c035809843a46d15
Devilpacks <mangyr97@mail.ru>
2023-04-24 13:20:10 +0300

    add singletone

---
+++ Creational/Singleton/index.ts
@@ -0,0 +1,23 @@
+class Settings {
+
+    static instance: Settings;
+    public readonly mode = 'dark';
+  
+    // prevent new with private constructor
+    private constructor() {
+  
+    }
+  
+    static getInstance(): Settings {
+      if (!Settings.instance) {
+        Settings.instance = new Settings();
+      }
+  
+      return Settings.instance;
+    }
+  
+  }
+  
+//   const settings = new Settings() // throws error
+  const settings = Settings.getInstance();
+  
