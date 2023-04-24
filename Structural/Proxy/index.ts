60cb2771c19b2f77d737a300de3ebfea5f09c409
Devilpacks <mangyr97@mail.ru>
2023-04-24 13:20:18 +0300

    add proxy

---
+++ Structural/Proxy/index.ts
@@ -0,0 +1,16 @@
+const original = { name: 'jeff' };
+
+const reactive = new Proxy(original, {
+  get(target, key) {
+    console.log('Tracking: ', key);
+    return target[key];
+  },
+  set(target, key, value) {
+    console.log('updating UI...');
+    return Reflect.set(target, key, value);
+  },
+});
+
+reactive.name; // 'Tracking: name'
+
+reactive.name = 'bob'; // 'updating UI...'
